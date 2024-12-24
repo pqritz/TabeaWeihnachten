d22ddf5d4c7bbbcadcf1aff98a3d6254e0eb3cd7
Pqritz <moritz.zeiler@gmx.de>
2024-12-24 01:12:48 +0100

    Init Commit Website mostly done, add download feature

---
+++ christmas.js
@@ -0,0 +1,72 @@
+document.getElementsByClassName("box")[0].addEventListener('click', () => {
+    const animatedDiv = document.getElementsByClassName('animStopper');
+    const boxgift = document.getElementsByClassName('box-gift')[0];
+    const applyAnimationOn = [document.getElementsByClassName('box-tape')[0], boxgift]
+    
+    boxgift.addEventListener('animationiteration', function handleIteration() {
+        for(i=0; i<animatedDiv.length; i++) {
+            animatedDiv[i].style.animationIterationCount = '1'; // Stop looping after the next iteration
+        }
+        boxgift.addEventListener('animationend', function handleAnimationEnd() {
+            for(i=0; i<animatedDiv.length; i++) {
+                animatedDiv[i].style.animation = 'none'; // Clear inline animation
+                //animatedDiv[i].classList.add('new-animation');
+            }
+            for(i=0;i<applyAnimationOn.length;i++) {
+                console.log(applyAnimationOn);
+                applyAnimationOn[i].style.animation = "openTop 1s linear forwards";
+            }
+            document.getElementsByClassName('box-gifts')[0].style.animation = "openBottom 1s linear forwards";
+            boxgift.removeEventListener('animationiteration', handleIteration);
+            boxgift.removeEventListener('animationend', handleAnimationEnd);
+        }, {once: true});
+    }, {once: true});
+    addRandomImage();
+}, {once:true})
+
+const imageUrls = [
+    "assets/bunnyHeart1.png",
+    "assets/bunnyHeart2.png",
+    "assets/bunnyHeart3.png",
+    "assets/bunnyHeart4.png",
+]
+
+function addRandomImage() {
+    const randomImage = imageUrls[Math.floor(Math.random() * imageUrls.length)];
+    const existingImage = document.querySelector('BunnyImage');
+    if(existingImage) {
+        existingImage.remove();
+    }
+    const img = document.createElement('img');
+    img.src = randomImage;
+    img.alt = 'Bunny Image';
+    img.className = 'BunnyImage';
+    img.style.animation = "bunnyImageAnim";
+    document.getElementsByClassName('imageContainer')[0].appendChild(img);
+    setTimeout(() => {
+        img.classList.add('visible');
+    }, 1000); // Small delay to ensure the image is added first
+}
+
+
+/** INCASE THE FANCY SHIT IS BETTER
+  @keyframes openAnimation {
+    from {
+        transform: translateY(0);
+    }
+    25% {
+        transform: translateY(-10em);
+    }
+    50% {
+        transform: translateY(-10em) translateX(-12.3em) rotate(-90deg);
+    }
+    65% {
+        transform: translateY(-10em) translateX(-12.3em) rotate(-180deg);
+    }
+    75% {
+        transform: translateY(5em) translateX(-12.3em) rotate(-180deg);
+    }
+    to {
+        transform: translateY(5em) translateX(-12.3em) rotate(-180deg);
+    }
+} */
